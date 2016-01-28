eb85ecb15c1f298ecf82d31db799f7d6cce7c899
Shane Keulen <shane.keulen@gmail.com>
2016-01-28 11:11:58 -0500

    wip

--- src/button/index.js
+++ src/button/index.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import BsButton from 'react-bootstrap/lib/Button';
-import styles from './button.css';
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
 
-console.log(styles);
+export class Button extends Component {
+  static propTypes = {
+    icon:      PropTypes.string,
+    type:      PropTypes.oneOf(['primary', 'secondary', 'important', 'success', 'danger', 'text', '']),
+    onClick:   PropTypes.func,
+    disabled:  PropTypes.bool,
+    inverse:   PropTypes.bool,
+    isSubmit:  PropTypes.bool,
+    style:     PropTypes.object,
+    classList: PropTypes.string,
+    sheet:     PropTypes.object
+  };
 
-class Button extends React.Component {
+  static defaultProps = {
+    disabled:  false,
+    inverse:   false,
+    isSubmit:  false,
+    classList: null,
+    sheet: { classes: {} }
+  };
+
   render() {
-    const cls = this.props.type === 'default' ? styles['btn-default'] : styles.btn;
-    return <BsButton className={cls}>{this.props.children}</BsButton>;
+    const { disabled, onClick, children, isSubmit } = this.props;
+
+    return <button type={isSubmit ? 'submit' : 'button'}
+      disabled={disabled}
+      className={this.getClasses()}
+      onClick={onClick}>
+      {children}
+    </button>;
   }
+
+  getClasses = () => {
+    const { disabled, inverse, type, children, classList, icon } = this.props;
+
+    return classNames(
+      'button',
+      !disabled && type,
+      {
+        disabled:   disabled,
+        iconButton: children && children.length === 0
+      },
+      icon && 'ic ic-' + icon,
+      !disabled && {
+        inverse: (type === 'secondary' || type === 'text') && inverse
+      },
+      classList
+    );
+  };
 }
 
 export default Button;
-
