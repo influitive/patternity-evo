d7b7ece96ea07a7bb91b94b55dd827cf806a0122
Nick Faulkner <nick.faulkner@influitive.com>
2016-07-07 14:53:15 -0400

    Added Modal using react bootstrap modal

---
+++ src/modal/index.js
@@ -0,0 +1,91 @@
+import React, { Component, PropTypes } from 'react';
+
+import BsModal from 'react-bootstrap/lib/Modal';
+
+import classes from './modal.css';
+
+export default class Modal extends Component {
+  static displayName = 'Modal';
+
+  static propTypes = {
+    class:         PropTypes.string,
+    closeable:     PropTypes.bool,
+    size:          PropTypes.oneOf(['small', 'large']),
+    onClose:       PropTypes.func,
+    keyboard:      PropTypes.bool,
+    animate:       PropTypes.bool
+  }
+
+  static defaultProps = {
+    class:         '',
+    closeable:     true,
+    size:          'large',
+    onClose:       function() {},
+    keyboard:      true,
+    animate:       false
+  }
+
+  render() {
+    return (
+      <BsModal
+        show={true}
+        onHide={this.props.onClose}
+        keyboard={this.props.keyboard}
+        animation={this.props.animate}
+        autoFocus={true}
+        backdrop={this._backdrop()}
+        bsSize={this.props.size}
+        dialogClassName={this.props.class}>
+        {this._renderChildren()}
+      </BsModal>
+    );
+  }
+
+  _backdrop() {
+    return this.props.closeable ? true : 'static';
+  }
+
+  _renderChildren() {
+    return React.Children.map(this.props.children,
+      (child) => {
+        if(child.type === Header){
+          return React.cloneElement(child, {
+            closeable: this.props.closeable,
+            onClose: this.props.onClose
+          });
+        }
+
+        return child;
+      }
+    );
+  }
+}
+
+export const Header = ({ children, closeable, onClose }) => {
+  return (
+    <BsModal.Header closeButton={closeable} onHide={onClose}>
+      <BsModal.Title>{children}</BsModal.Title>
+    </BsModal.Header>
+  );
+};
+
+Header.propTypes = {
+  closeable: PropTypes.bool,
+  onClose: PropTypes.func
+};
+
+export const Body = ({ children }) => {
+  return (
+    <BsModal.Body>
+      {children}
+    </BsModal.Body>
+  );
+};
+
+export const Footer = ({ children }) => {
+  return (
+    <BsModal.Footer>
+      {children}
+    </BsModal.Footer>
+  );
+};
---
+++ src/modal/modal.card.js
@@ -0,0 +1,55 @@
+/*eslint max-len: 0*/
+import devboard from 'devboard';
+import React from 'react';
+
+import Modal, { Header, Body, Footer } from './index.js';
+import { Button } from '../button';
+
+const devcard = devboard.ns('Modal');
+
+devcard(
+  'Modal',
+  'Example of Modal Dialog Component',
+  ({state}) => <div>
+      {modal(state)}
+      <Button onClick={({ target }) => state.swap(set('isOpen', true))}>Show Modal</Button>
+    </div>
+  ,
+  {
+    state: devboard.atom({
+      isOpen: false
+    }),
+  }
+);
+
+const modal = (state) => {
+  if (!state.deref().isOpen) return null;
+
+  return (
+    <Modal onClose={() => state.swap(set('isOpen', false))}>
+      <Header>My Modal</Header>
+      <Body>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Body>
+      <Footer>
+        <Button onClick={() => state.swap(set('isOpen', false))}>Cancel</Button>
+        <span style={{'padding-left': '10px'}}>
+          <Button onClick={() => state.swap(set('isOpen', false))} type="success">Ok</Button>
+        </span>
+      </Footer>
+    </Modal>
+  )
+};
+
+const set = (key, val) => {
+  return obj => Object.assign(
+    {}, obj,
+    { [key]: (typeof val === 'function') ? val(obj[key]) : val }
+  );
+};
