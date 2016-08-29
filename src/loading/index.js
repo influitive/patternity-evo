61c1677483334cfdc93a76d55bb512c53e688317
Shane Keulen <shane.keulen@gmail.com>
2016-08-29 16:56:23 -0400

    use classnames
    use em!

--- src/loading/index.js
+++ src/loading/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import cn from 'classnames';
 
 import styles from './styles.css';
 
-const modelCheck = (isModal) => isModal ? 'isModal' : '';
-
-const Loading = ({ size = 'medium', display = '', type = 'dark', isModal = false }) => (
-  <span className={`${styles.loadingSpinner}
-    ${styles[size]}
-    ${styles[display]}
-    ${styles[type]}
-    ${styles[modelCheck(isModal)]}`}></span>
+export default ({ size = 'medium', display = '', type = 'dark', isModal = false }) => (
+  <span className={cn(
+    styles.loadingSpinner,
+    styles[size],
+    styles[display],
+    styles[type],
+    { [styles.isModal]: isModal }
+  )}></span>
 );
-
-export default Loading;
