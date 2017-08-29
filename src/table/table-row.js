7d93755614aa85cd07e68036f2e4847f9e01a265
Shane Keulen <shane.keulen@gmail.com>
2017-08-29 12:05:22 -0400

    remove shitty components

--- src/table/table-row.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classnames from 'classnames';
-import styles from './table.css';
-
-export default ({ style, className, ...props }) =>
-  <div
-    className={classnames('pt-table-row', styles.row, className)} {...props}
-    style={style}>
-  </div>;
