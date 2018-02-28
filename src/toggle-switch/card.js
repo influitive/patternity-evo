abe64bf1eb2a718e9d80cd4edbc071c0d6676686
Shane Keulen <shane.keulen@gmail.com>
2018-02-28 14:37:35 -0500

    fix missing themeprovider on toggleswitch card

--- src/toggle-switch/card.js
+++ src/toggle-switch/card.js
@@ -1,6 +1,7 @@
 import devboard from 'devboard';
 import React from 'react';
 import ToggleSwitch from './index.js';
+import { ThemeProvider } from 'gild';
 
 const devcard = devboard.ns('toggleswitch');
 
@@ -15,34 +16,36 @@ devcard(
   * default labels, active true, disabled
   `,
   ({ state, setState }) => (
-    <div style={{ display: 'flex' }}>
-      <div style={{ margin: '5px' }}>
-        <ToggleSwitch
-          value={state.toggle1}
-          onChange={() => setState({ toggle1: !state.toggle1 })}/>
+    <ThemeProvider theme={{ themeColorPrimary: 'blue' }}>
+      <div style={{ display: 'flex' }}>
+        <div style={{ margin: '5px' }}>
+          <ToggleSwitch
+            value={state.toggle1}
+            onChange={() => setState({ toggle1: !state.toggle1 })}/>
+        </div>
+        <div style={{ margin: '5px' }}>
+          <ToggleSwitch
+            value={state.toggle2}
+            trueLabel="Yes, sign me up!"
+            falseLabel="No"
+            onChange={() => setState({ toggle2: !state.toggle2 })}/>
+        </div>
+        <div style={{ margin: '5px' }}>
+          <ToggleSwitch
+            value={state.toggle3}
+            changeColor={false}
+            trueLabel="all"
+            falseLabel="any"
+            onChange={() => setState({ toggle3: !state.toggle3 })}/>
+        </div>
+        <div style={{ margin: '5px' }}>
+          <ToggleSwitch
+            enabled={false}
+            value={state.toggle4}
+            onChange={() => setState({ toggle4: !state.toggle4 })}/>
+        </div>
       </div>
-      <div style={{ margin: '5px' }}>
-        <ToggleSwitch
-          value={state.toggle2}
-          trueLabel="Yes, sign me up!"
-          falseLabel="No"
-          onChange={() => setState({ toggle2: !state.toggle2 })}/>
-      </div>
-      <div style={{ margin: '5px' }}>
-        <ToggleSwitch
-          value={state.toggle3}
-          changeColor={false}
-          trueLabel="all"
-          falseLabel="any"
-          onChange={() => setState({ toggle3: !state.toggle3 })}/>
-      </div>
-      <div style={{ margin: '5px' }}>
-        <ToggleSwitch
-          enabled={false}
-          value={state.toggle4}
-          onChange={() => setState({ toggle4: !state.toggle4 })}/>
-      </div>
-    </div>
+    </ThemeProvider>
   ),
   {
     state: {
